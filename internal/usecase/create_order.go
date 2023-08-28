9a40bd4306288236cd03e33bfd3714f11cb8800a
weslyramalho <weslyramalho@yahoo.com.br>
2023-08-28 14:53:02 -0300

    first commit

---
+++ internal/usecase/create_order.go
@@ -0,0 +1,59 @@
+package usecase
+
+import (
+	"github.com/weslyramalho/GO/tree/main/clean_arch/internal/entity"
+	"github.com/weslyramalho/GO/tree/main/clean_arch/pkg/events"
+)
+
+type OrderInputDTO struct {
+	ID    string  `json:"id"`
+	Price float64 `json:"price"`
+	Tax   float64 `json:"tax"`
+}
+
+type OrderOutputDTO struct {
+	ID         string  `json:"id"`
+	Price      float64 `json:"price"`
+	Tax        float64 `json:"tax"`
+	FinalPrice float64 `json:"final_price"`
+}
+
+type CreateOrderUseCase struct {
+	OrderRepository entity.OrderRepositoryInterface
+	OrderCreated    events.EventInterface
+	EventDispatcher events.EventDispatcherInterface
+}
+
+func NewCreateOrderUserCase(
+	OrderRepository entity.OrderRepositoryInterface,
+	OrderCreated events.EventInterface,
+	EventDispatcher events.EventDispatcherInterface,
+) *CreateOrderUseCase {
+	return &CreateOrderUseCase{
+		OrderRepository: OrderRepository,
+		OrderCreated:    OrderCreated,
+		EventDispatcher: EventDispatcher,
+	}
+}
+
+func (c *CreateOrderUseCase) Execute(input OrderInputDTO) (OrderOutputDTO, error) {
+	order := entity.Order{
+		ID:    input.ID,
+		Price: input.Price,
+		Tax:   input.Tax,
+	}
+	order.CalculateFinalPrice()
+	if err := c.OrderRepository.Save(&order); err != nil {
+		return OrderOutputDTO{}, err
+	}
+	dto := OrderOutputDTO{
+		ID:         order.ID,
+		Price:      order.Price,
+		Tax:        input.Tax,
+		FinalPrice: order.FinalPrice,
+	}
+	c.OrderCreated.SetPayload(dto)
+	c.EventDispatcher.Dispatch(c.OrderCreated)
+	return dto, nil
+
+}
